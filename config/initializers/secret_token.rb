877f9a63f1c3d97c3e0f8f76cd4bf80faee4b1d1
Sam <satiricallaughter@gmail.com>
2013-06-18 20:52:08 +0100

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OperationTshirt::Application.config.secret_token = 'c809b5f7b6e2e10a04805ae0941f2c44aa9d753c478100290edc75131b675223be2c67a6777eb6101f8a0639b78f2e9ef70bb18079a3f5e5a2431f696499aa41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OperationTshirt::Application.config.session_store :cookie_store, key: '_operation_tshirt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OperationTshirt::Application.config.session_store :active_record_store
